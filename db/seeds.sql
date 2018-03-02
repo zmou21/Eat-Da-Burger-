c90525a8b2e1e6983b3b4208fb212ce3d8072a72
Zack <zmoumen13@gmail.com>
2018-03-02 12:30:10 -0700

    updated with database values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,5 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name) VALUES ('Egg and Peanut Butter Burger');
+INSERT INTO burgers (burger_name) VALUES ('Plain Cheeseburger');
+INSERT INTO burgers (burger_name, sleepy) VALUES ('Pizza Burger', true);
+INSERT INTO burgers (burger_name, sleepy) VALUES ('Triple Cheeseburger', true);
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Burger');
