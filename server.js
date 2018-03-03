b64e31fc730c673a6ba15f582e700af5260af6f3
Zack <zmoumen13@gmail.com>
2018-03-02 18:53:04 -0700

    updated with accurate controller file

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+  var express = require("express");
+  var bodyParser = require("body-parser");
 
-var port = 3000;
+  var port = 3000;
 
-var app = express();
+  var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+  // Serve static content for the app from the "public" directory in the application directory.
+  app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.urlencoded({ extended: false }));
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+  // Set Handlebars.
+  var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+  app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+  app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+  // Import routes and give the server access to them.
+  var route = require("./controllers/burgers_controller.js");
 
-app.use("/", routes);
+  app.use("/", route);
 
-app.listen(port);
+  app.listen(port);
