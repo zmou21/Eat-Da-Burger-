1fcd637240bd5dc9ae5231062ca48b747431979e
Zack <zmoumen13@gmail.com>
2018-03-02 18:55:29 -0700

    initial commits

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,53 @@
+  var express = require("express");
+
+  var router = express.Router();
+
+  // Import the model (burger.js) to use its database functions.
+  var burger = require("../models/burger.js");
+
+  // Create all our routes and set up logic within those routes where required.
+  router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+      var hbsObject = {
+        burger_name: data
+      };
+      //console.log("This is working...controller.js");
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+
+  router.post("/api/burgers", function(req, res) {
+    //var burgerName = req.body.burger_name;
+    burger.insertOne([
+      "burger_name"
+    ], [
+      req.body.burger_name
+    ], function(result) {
+      // Send back the ID of the new quote
+      console.log("burger added ... controller.js");
+      console.log(result);
+      res.json({ burger_name: result  });
+    });
+  });
+
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne({
+      burger_name: req.body.burger_name
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+
+
+  // Export routes for server.js to use.
+  module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,27 @@
+  // Import the ORM to create functions that will interact with the database.
+  var orm = require("../config/orm.js");
+
+  var burger = {
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        //console.log("Thi is working...burger.js");
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function(table, cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        console.log("burger added ... burger.js");
+
+        cb(res);
+      });
+    },
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+
+  // Export the database functions for the controller (burgersController.js).
+  module.exports = burger;
